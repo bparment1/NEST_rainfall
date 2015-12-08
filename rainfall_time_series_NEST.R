0ef4e4b1095e4764c30ac6f25679f09e3202fc09
Benoit Parmentier <benoit.parmentier@gmail.com>
2015-12-08 10:45:00 -0500

    adding options to process extraction of rainfall by year and processing of bacteria measurements

--- rainfall_time_series_NEST.R
+++ rainfall_time_series_NEST.R
@@ -5,7 +5,7 @@
 
 #AUTHORS: Benoit Parmentier                                             
 #DATE CREATED: 11/05/2015 
-#DATE MODIFIED: 11/23/2015
+#DATE MODIFIED: 12/08/2015
 #Version: 1
 #PROJECT: NEST beach closures            
 
@@ -84,7 +84,7 @@ CRS_reg <- CRS_WGS84 # PARAM 3
 file_format <- ".rst" #PARAM 4
 NA_value <- -9999 #PARAM5
 NA_flag_val <- NA_value #PARAM6
-out_suffix <-"NEST_prism_11232015" #output suffix for the files and ouptu folder #PARAM 7
+out_suffix <-"NEST_prism_12082015" #output suffix for the files and ouptu folder #PARAM 7
 create_out_dir_param=TRUE #PARAM8
 num_cores <- 11 #PARAM 9
 
@@ -96,8 +96,9 @@ end_date <- "2012-12-31" #PARAM 13
 var_name <- "COL_SCORE" #PARAM 14
 var_ID <- "LOCATION_ID" #PARAM 15
 year_processed <- "2012" #PARAM 16
-threshold_val <- 2 #PARAM 17
-convert_to_inches <- TRUE #PARAM 18
+threshold_val <- 2*25.4 #PARAM 17, in inches or mm
+convert_to_inches <- FALSE #PARAM 18
+units_val <- "mm"
 
 ################# START SCRIPT ###############################
 
@@ -116,7 +117,7 @@ if(create_out_dir_param==TRUE){
 
 r_rainfall <- stack(mixedsort(list.files(pattern="*.tif",path=file.path(in_dir,rainfall_dir),full.names=T))) #rainfall time series stack
 
-#### Make this a function...that will run automatically the predictions
+#### Read in data and add time flag
 
 plot(r_rainfall,y=1)
 data <- read.table(station_data_fname,sep=",",header=T) #this is T-mode using cor matrix
@@ -126,11 +127,17 @@ data$TRIP_START_DATE_f <- as.Date(strptime(dates_TRIP_START,"%m/%d/%Y"))
 data$TRIP_START_DATE_month <- strftime(data$TRIP_START_DATE_f , "%m") # current month of the date being processed
 data$TRIP_START_DATE_year <- strftime(data$TRIP_START_DATE_f , "%Y")
 data$TRIP_START_DATE_day <- strftime(data$TRIP_START_DATE_f , "%d")
+dim(data)
+class(data$LOCATION_ID)
+data$LOCATION_ID <- as.character(data$LOCATION_ID)
+length(unique(data$LOCATION_ID)) #There are 2851 stations
 
 if (convert_to_inches==TRUE){
-
   r_rainfall <- r_rainfall/25.4 #improve efficiency later? YES!!
 }
+
+
+#### NOW SELECT RELEVANT DATES
 
 idx <- seq(as.Date(start_date), as.Date(end_date), 'day')
 #date_l <- strptime(idx[1], "%Y%m%d") # 
@@ -139,6 +146,7 @@ dates_l <- format(idx, "%Y%m%d") #  date being processed
 data_subset <- data[data$TRIP_START_DATE_f >= as.Date(start_date) & data$TRIP_START_DATE_f <= as.Date(end_date), ]
 data_subset$LOCATION_ID <- as.character(data_subset$LOCATION_ID)
 
+#Remote stations without coordinates and make a SPDF
 dat_stat <- subset(data_subset, !is.na(LONGITUDE_DECIMAL) & !is.na(LATITUDE_DECIMAL))
 coords <- dat_stat[,c('LONGITUDE_DECIMAL','LATITUDE_DECIMAL')]
 coordinates(dat_stat) <- coords
@@ -151,7 +159,7 @@ nrow(dat_stat)==length(unique(dat_stat$LOCATION_ID)) #Checking that we have a un
 
 r_rainfall <- setZ(r_rainfall, idx) #for now, this can also be made into a spacetime object
 
-x <- zApply(r_rainfall, by="day",fun=mean,name="overall mean") #overall mean
+x <- zApply(r_rainfall, by="day",fun=mean,name="overall mean") #overall mean, takes about a minute
 raster_name <- paste("day","_","overall_mean",file_format,sep="")
 writeRaster(x, file=raster_name,overwrite=T)
 plot_to_file(raster_name) #quick plot of raster to disk
@@ -163,6 +171,8 @@ r_date <- getZ(r_rainfall)
 ## Get pixel time series at centroids of tiles used in the predictions
 
 df_ts_pixel <- extract(r_rainfall,dat_stat,df=T,sp=T)
+test<-merge(df_ts_pixel,data_subset,by="LOCATION_ID")
+
 #df_ts_pixel <- cbind(summary_metrics_v,df_ts_pixel)
 r_ts_name <- names(r_rainfall)
 #d_z <- zoo(df_ts_pixel,idx) #make a time series .
@@ -178,6 +188,13 @@ plot(table(data_subset$LOCATION_ID),type="h", main="Number of measurements",
      ylab="Frequency of coliform measurements",xlab="Station ID")
 
 dev.off()
+
+#hist(table(data_subset$COL_SCORE))
+#Could output in a textfile
+range(table(dat_stat$LOCATION_ID)) #1 to 60
+mean(table(dat_stat$LOCATION_ID)) #8
+median(table(data_subset$LOCATION_ID)) #8
+hist(table(data_subset$LOCATION_ID), main="Frequency of Number of measurements by stations")
 
 ref_pol <- create_polygon_from_extent(dat_stat,outDir=out_dir,outSuffix=out_suffix)
   
@@ -219,24 +236,25 @@ dev.off()
 #
 
 freq_station <- sort(table(data_subset$LOCATION_ID),decreasing=T) # select top 2 stations in term of availability
-list_selected_ID <- names(freq_station)[1:2]
+list_selected_ID <- names(freq_station)[1:2] #select top 2
+#View(freq_station)
 
 ##This will be a function later on...
-df_ts_pix <- df_ts_pixel
+df_ts_pix <- df_ts_pixel#this contains the pixels with extracted pixels
 #list_selected_pix <- 11:14
 list_pix <- vector("list",length=length(list_selected_ID))
 #threshold <- 
 
-for(i in 1:length(list_selected_ID)){
-  
-  id_name <- list_selected_ID[i]
 
-  #id_selected <- data_subset[[var_ID]]==id_name
+plotting_coliform_and_rainfall <- function(i,df_ts_pix,list_selected_ID,plot_fig=T){
+  
+#for(i in 1:length(list_selected_ID)){
+  
+  #get the relevant station
+  id_name <- list_selected_ID[i] # e.g. WS037.00
   id_selected <- df_ts_pix[[var_ID]]==id_name
-  #df_ts_pix[id_selected,]
-  
+
   ### Not get the data from the time series
-  
   data_pixel <- df_ts_pix[id_selected,]
   var_pix_ts <- t(as.data.frame(subset(data_pixel,select=var_name)))
   #pix <- t(data_pixel[1,24:388])#can subset to range later
@@ -264,96 +282,110 @@ for(i in 1:length(list_selected_ID)){
   
   df2 <- as.data.frame(d_z2)
   df2$COL_SCORE <- as.numeric(as.character(df2$COL_SCORE))
+  df2$rainfall <- as.numeric(as.character(df2$rainfall))
+  
   #plot(df2$rainfall)
   
 
   list_pix[[i]] <- pix_ts
   
-  res_pix <- 480
-  col_mfrow <- 2
-  row_mfrow <- 1
+  if(plot_fig==T){
 
-  #png(filename=paste("Figure3a_","pixel_profile_",id_name,"_",out_suffix,".png",sep=""),
-  #    width=col_mfrow*res_pix,height=row_mfrow*res_pix)
+    res_pix <- 480
+    col_mfrow <- 2
+    row_mfrow <- 1
+    
+    ###
+    #Figure 3b
+    png(filename=paste("Figure3b_","pixel_profile_var_combined_",id_name,"_",out_suffix,".png",sep=""),
+        width=col_mfrow*res_pix,height=row_mfrow*res_pix)
+    
+    #plot(d_z,lty=2,ylab="rainfall",xlab="Time",main="")
+    #points(d_z2$COL_SCORE,col="red",pch=10,cex=2)
+    plot(d_z,lty=2,ylab="rainfall",xlab="Time",main="")
+    abline(h=threshold_val,col="green")
+    
+    par(new=TRUE)              # key: ask for new plot without erasing old
+    #plot(x,y,type="l",col=t_col[k],xlab="",ylab="",lty="dotted",axes=F) #plotting fusion profile
+    plot(df2$COL_SCORE,pch=10,cex=2.5,col="red", axes=F,ylab="",xlab="")
+    #points(d_z2$COL_SCORE,col="red",pch=10,cex=2)
+    legend("topleft",legend=c("stations"), 
+           cex=1.2,col="red",pch =10,bty="n")
+    
+    axis(4,cex=1.2)
+    mtext(4, text = "coliform scores", line = 3)
+    
+    title(paste("Station time series",id_name,sep=" "))
+    
+    dev.off()
+    
+    #Figure 3c
+    png(filename=paste("Figure3c_","pixel_profile_var_combined_log_scale_",id_name,"_",out_suffix,".png",sep=""),
+        width=col_mfrow*res_pix,height=row_mfrow*res_pix)
+    
+    #plot(d_z,lty=2,ylab="rainfall",xlab="Time",main="")
+    #points(d_z2$COL_SCORE,col="red",pch=10,cex=2)
+    plot(d_z,lty=2,ylab="rainfall",xlab="Time",main="")
+    abline(h=threshold_val,col="green")
+    par(new=TRUE)              # key: ask for new plot without erasing old
+    #plot(x,y,type="l",col=t_col[k],xlab="",ylab="",lty="dotted",axes=F) #plotting fusion profile
+    plot(log(df2$COL_SCORE),pch=10,cex=2.5,col="red", axes=F,ylab="",xlab="")
+    
+    #points(d_z2$COL_SCORE,col="red",pch=10,cex=2)
+    legend("topleft",legend=c("stations"), 
+           cex=1.2,col="red",pch =10,bty="n")
+    
+    axis(4,cex=1.2)
+    mtext(4, text = "coliform scores", line = 3)
+    
+    title(paste("Station time series",id_name,sep=" "))
+    
+    dev.off()
+    
+    ####Histogram of values
+    
+    res_pix <- 480
+    col_mfrow <- 2
+    row_mfrow <- 1
+    
+    png(filename=paste("Figure4_","histogram_coliform_measurements_",year_processed,"_",id_name,"_",out_suffix,".png",sep=""),
+        width=col_mfrow*res_pix,height=row_mfrow*res_pix)
+    
+    hist_val <- hist(df2$COL_SCORE,main="",xlab="COLIFORM SCORES")
+    title(paste("Histrogram of coliform scores for station",id_name,"in",year_processed,sep=" "))
+    #abline(v=threshold_val,col="green" )
+    legend("topright",legend=c("treshold val"), 
+           cex=1.2, col="green",lty =1,bty="n")  
+    
+    y_loc <- max(hist_val$counts)/2
+    
+    #text(threshold_val,y_loc,paste(as.character(threshold_val)),pos=1,offset=0.1)
+    
+    dev.off()
+    
+    plot(df2$rainfall)
+    plot(df2$rainfall,df2$COL_SCORE)
+    plot(df2$rainfall,log(df2$COL_SCORE))
+    
+    
+  }
   
-  #plot(d_z2,type="b",main="",pch=10)
-  #title(paste("Pixel time series",id_name,sep=" "))
-  
-  #dev.off()
-  
-  ###
-  #Figure 3b
-  png(filename=paste("Figure3b_","pixel_profile_var_combined_",id_name,"_",out_suffix,".png",sep=""),
-      width=col_mfrow*res_pix,height=row_mfrow*res_pix)
-  
-  #plot(d_z,lty=2,ylab="rainfall",xlab="Time",main="")
-  #points(d_z2$COL_SCORE,col="red",pch=10,cex=2)
-  plot(d_z,lty=2,ylab="rainfall",xlab="Time",main="")
-  par(new=TRUE)              # key: ask for new plot without erasing old
-  #plot(x,y,type="l",col=t_col[k],xlab="",ylab="",lty="dotted",axes=F) #plotting fusion profile
-  plot(df2$COL_SCORE,pch=10,cex=2.5,col="red", axes=F,ylab="",xlab="")
-  abline(h=threshold_val,col="green")
-  #points(d_z2$COL_SCORE,col="red",pch=10,cex=2)
-  legend("topleft",legend=c("stations"), 
-         cex=1.2,col="red",pch =10,bty="n")
-
-  axis(4,cex=1.2)
-  mtext(4, text = "coliform scores", line = 3)
-  
-  title(paste("Station time series",id_name,sep=" "))
-  
-  dev.off()
-  
-  #Figure 3c
-  png(filename=paste("Figure3c_","pixel_profile_var_combined_log_scale_",id_name,"_",out_suffix,".png",sep=""),
-      width=col_mfrow*res_pix,height=row_mfrow*res_pix)
-  
-  #plot(d_z,lty=2,ylab="rainfall",xlab="Time",main="")
-  #points(d_z2$COL_SCORE,col="red",pch=10,cex=2)
-  plot(d_z,lty=2,ylab="rainfall",xlab="Time",main="")
-  par(new=TRUE)              # key: ask for new plot without erasing old
-  #plot(x,y,type="l",col=t_col[k],xlab="",ylab="",lty="dotted",axes=F) #plotting fusion profile
-  plot(log(df2$COL_SCORE),pch=10,cex=2.5,col="red", axes=F,ylab="",xlab="")
-  abline(h=threshold_val,col="green")
-  #points(d_z2$COL_SCORE,col="red",pch=10,cex=2)
-  legend("topleft",legend=c("stations"), 
-         cex=1.2,col="red",pch =10,bty="n")
-  
-  axis(4,cex=1.2)
-  mtext(4, text = "coliform scores", line = 3)
-  
-  title(paste("Station time series",id_name,sep=" "))
-  
-  dev.off()
-  
-  ####Histogram of values
-  
-  res_pix <- 480
-  col_mfrow <- 2
-  row_mfrow <- 1
-  
-  png(filename=paste("Figure4_","histogram_coliform_measurements_",year_processed,"_",id_name,"_",out_suffix,".png",sep=""),
-      width=col_mfrow*res_pix,height=row_mfrow*res_pix)
-  
-  hist_val <- hist(df2$COL_SCORE,main="",xlab="COLIFORM SCORES")
-  title(paste("Histrogram of coliform scores for station",id_name,"in",year_processed,sep=" "))
-  abline(v=threshold_val,col="green" )
-  legend("topright",legend=c("treshold val"), 
-         cex=1.2, col="green",lty =1,bty="n")  
-  
-  y_loc <- max(hist_val$counts)/2
-  
-  #text(threshold_val,y_loc,paste(as.character(threshold_val)),pos=1,offset=0.1)
-  
-  dev.off()
   
   
   ## Now correlation.
-  
+  #sum(is.na(df2$rainfall))
+  #[1] 0
+  nb_zero <- sum((df2$rainfall==0)) #203
+  nb_NA <- sum(is.na(df2$COL_SCORE))
   ## Cumulated precip and lag?
-  
+  #Keep number of  0 for every year for rainfall
+  #summarize by month
+  #Kepp number of NA for scores... 
+  #Summarize by season...
   ## Threshold?
-  
+  station_summary_obj <- list(nb_zero,nb_NA)
+  names(station_summary_obj) <- c("nb_zero","nb_NA")
+  return(station_summary_obj)
 }
 
 data_dz <- do.call(cbind,list_pix)
