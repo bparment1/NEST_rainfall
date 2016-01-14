33a4940f14170272b027b2266f10fef7cd3e8787
Benoit Parmentier <benoit.parmentier@gmail.com>
2016-01-14 10:45:00 -0500

    modifying script for downloading into a function

--- downloading_NEST_data.R
+++ downloading_NEST_data.R
@@ -1,77 +1,60 @@
+###Loading R library and packages                                                      
+#library(gtools)    # loading some useful tools 
 
+library(sp)
+library(raster)
+library(rgdal)
+require(rgeos)
+library(BMS) #contains hex2bin and bin2hex
+library(bitops)
+require(RCurl)
+require(stringr)
+require(XML)
 
-http://services.nacse.org/prism/data/public/4km/tmin/20090405
-
-wget --content-disposition http://services.nacse.org/prism/data/public/4km/tmin/20090405
-
-
-st <- as.Date(start_date,format="%Y.%m.%d") #start date
-en <- as.Date(end_date,format="%Y.%m.%d") #end date
-ll <- seq.Date(st, en, by="1 day") #sequence of dates
-dates_queried <- format(ll,"%Y.%m.%d") #formatting queried dates
-
-url_product <-paste("http://e4ftl01.cr.usgs.gov/MOLT/",MODIS_product,"/",sep="") #URL is a constant...
-#url_product <- file.path("http://e4ftl01.cr.usgs.gov/MOLT/",MODIS_product)
-
-dates_available <- extractFolders(url_product)  #Get the list of available driectory dates for the product, from 2000 to now
-
-list_folder_dates <- intersect(as.character(dates_queried), as.character(dates_available)) #list of remote folders to access
-#list_folder_dates <-setdiff(as.character(dates_available), as.character(dates_queried))
-
-#step 2: list content of specific day folder to obtain specific file...  #parse by tile name!!!
-
-url_folders_str <-paste(url_product,list_folder_dates,"/",sep="") #url for the folders matching dates to download
-
-## loop over
-#debug(extractFiles)
-#generate outdir for each tile!!!
-
-list_folders_files <- vector("list",length(url_folders_str))
-d_files <- vector("list",length(list_folders_files))
-file_format<-c("hdf","xml")
-#can make this faster using parallelization...
-for (i in 1:length(url_folders_str)){
-  #debug(extractFiles)
-  list_folders_files[[i]] <- try(extractFiles(url_folders_str[i], list_tiles)[file_format]) 
-  #d_files[[i]] <- list_folders_files[[i]][[file_format]]                      
-}
-#list_folders_files <- lapply(url_folders_str,extractFiles,list_tiles_str=list_tiles)
-#Now remove error objects...
-d_files_tmp <-remove_from_list_fun(l_x=list_folders_files,condition_class ="try-error")
-d_files <- as.character(unlist(d_files_tmp$list)) #all the files to download...
-n_dir_available <- length(d_files)
-n_dir_requested <- length(list_folders_files)
-
-#Step 3: download file to the directory 
-#browser()
-#prepare files and directories for download
-out_dir_tiles <- file.path(out_dir,list_tiles)
-list_files_tiles <- vector("list",length(list_tiles))
-for(j in 1:length(out_dir_tiles)){
-  if (!file.exists(out_dir_tiles[j])){
-    dir.create(out_dir_tiles[j])
+function(start_date, end_date,var_name,num_cores){
+  #
+  download_prism <- function(date_selected,var_name){
+    url_product <- "http://services.nacse.org/prism/data/public/4km/" #URL is a constant...
+    var_name <- "tmin" #tmax,ppt
+    url_product_var <- paste(url_product,var_name,sep="")
+    #system("wget --content-disposition $base_url/$clim_var/$day");
+    cmd_str <- paste("wget --content-disposition ",url_product_var,date_selected,sep="")
+    df <- data.frame(var=var_name,date=date_selected,url=url_product)
+    return(df)
   }
-  list_files_tiles[[j]] <- grep(pattern=list_tiles[j],x=d_files,value=TRUE) 
+  
+  start_date <- as.Date(start_date,format="%Y/%m/%d") #start date
+  end_date <- as.Date(end_date,format="%Y/%m/%d") #end date
+  dates_range <- seq.Date(start_date, end_date, by="1 day") #sequence of dates
+  
+  l_df_download <- mclapply(dates_range,FUN=download_prism,var_name=var_name,
+                            mc.preschedule=FALSE,mc.cores = num_cores)
+  
+  #wget http://services.nacse.org/prism/data/public/4km/tmin/20090405
+  #
+  #
 }
 
-#Now download per tiles: can be parallelized
-for (j in 1:length(list_files_tiles)){ #loop around tils
-  file_items <- list_files_tiles[[j]]
-  for (i in 1:length(file_items)){
-    file_item <- file_items[i]
-    download.file(file_item,destfile=file.path(out_dir_tiles[j],basename(file_item)))
-    #download.file(file_item,destfile="test.hdf")
-  }
-}
+#################### END OF SCRIPT #####################
 
-#Prepare return object: list of files downloaded with http and list downloaded of files in tiles directories
+# use strict;
+# use warnings;
+# use DateTime;
+# day <- 
+# my $clim_var = 'ppt';
+# my $base_url = 'http://services.nacse.org/prism/data/public/4km';
+# my $start = DateTime->new( day => 1, month => 10, year => 1999 );
+# my $stop = DateTime->new( day => 30, month => 9, year => 2000 );
+# while($start <= $stop) {
+#   $day = $start->strftime('%Y%m%d'); #place date in proper form
+#   system("wget --content-disposition $base_url/$clim_var/$day");
+#   sleep 2; #to be nice to our server
+#   $start->add(days => 1);
+# }
+# 
+# 
+# dates_queried <- format(ll,"%Y.%m.%d") #formatting queried dates
+# 
 
-list_files_by_tiles <-mapply(1:length(out_dir_tiles),FUN=function(i,x){list.files(path=x[[i]],pattern="*.hdf$",full.names=T)},MoreArgs=(list(x=out_dir_tiles))) #Use mapply to pass multiple arguments
-#list_files_by_tiles <-mapply(1:length(out_dir_tiles),FUN=list.files,MoreArgs=list(pattern="*.hdf$",path=out_dir_tiles,full.names=T)) #Use mapply to pass multiple arguments
-
-colnames(list_files_by_tiles) <- list_tiles #note that the output of mapply is a matrix
-download_modis_obj <- list(list_files_tiles,list_files_by_tiles)
-names(download_modis_obj) <- c("downloaded_files","list_files_by_tiles")
-return(download_modis_obj)
 
 
