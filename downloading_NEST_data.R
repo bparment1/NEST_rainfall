b9d4fbae97df652021054a6fe64e31f6c42fec6d
Benoit Parmentier <benoit.parmentier@gmail.com>
2016-01-22 10:45:00 -0500

    processing of downlaoded data  by using matching region and aggregation and reprojection if necessary

--- downloading_NEST_data.R
+++ downloading_NEST_data.R
@@ -11,24 +11,37 @@
 #
 #COMMENTS: -   
 #          - 
-#TO DO:
+#TO DO:ESSSS
 # -make this callable from shell?
 #
 #################################################################################################
 
 ###Loading R library and packages                                                      
 
-library(parallel)
-library(sp)
-library(raster)
-library(rgdal)
-require(rgeos)
-library(BMS) #contains hex2bin and bin2hex
-library(bitops)
+library(raster)                 # loading the raster package
+library(gtools)                 # loading R helper programming tools/functions
+library(sp)                     # spatial objects in R
+library(gplots)                 # plotting functions such as plotCI
+library(rgdal)                  # gdal driver for R
+library(RColorBrewer)           # color scheme, palettes used for plotting
+library(gdata)                  # read different format (including .xlsx)
+library(plotrix)                # plot options and functions 
+library(rasterVis)              # raster visualization
+library(colorRamps)             # contains matlab.like palette
+library(zoo)                    # time series objects and methods
+library(xts)                    # extension of time series objects
+library(BMS)                    # contains hex2bin and bin2hex
+library(bitops)                 # bit operations
+library(gtools)                 #
+library(maptools)               #
+library(rgeos)                  # spatial analysis, topological and geometric operations e.g. interesect, union, contain etc.
+library(sphet)                  # spatial analyis, regression eg.contains spreg for gmm estimation
+library(forecast)               # arima and other time series methods
+library(lubridate)              # date and time handling tools
+library(parallel)               # access to parallelization functions
 require(RCurl)
 require(stringr)
 require(XML)
-library(lubridate)
 
 ###### Functions used in this script sourced from other files
 
@@ -125,6 +138,170 @@ downloading_prism_product <- function(start_date, end_date,var_name,num_cores,ou
   return(df_downloaded)
 }
 
+#This function creates a spatial polygon data frame object for the extent matching a raster input
+create_polygon_from_extent<-function(reg_ref_rast,outDir=NULL,outSuffix=NULL){
+  #This functions returns polygon sp from input rast
+  #Input Arguments: 
+  #reg_ref_rast: input ref rast
+  #outDir : output directory, if NULL then the current dir in used
+  #outSuffix: output suffix used for the naming of the shapefile
+  #Output: 
+  #reg_outline_poly: spatial polygon data.frame
+  #
+  if(is.null(outDir)){
+    outDir=getwd()
+  }
+  if(is.null(outSuffix)){
+    outSuffix=""
+  }
+  ref_e <- extent(reg_ref_rast) #extract extent from raster object
+  reg_outline_poly <- as(ref_e, "SpatialPolygons") #coerce raster extent object to SpatialPolygons from sp package 
+  reg_outline_poly <- as(reg_outline_poly, "SpatialPolygonsDataFrame") #promote to spdf
+  proj4string(reg_outline_poly) <- projection(reg_ref_rast) #Assign projection to spdf
+  infile_reg_outline <- paste("reg_out_line_",out_suffix,".shp",sep="") #name of newly crated shapefile with the extent
+  writeOGR(reg_outline_poly,dsn= outDir,layer= sub(".shp","",infile_reg_outline), 
+           driver="ESRI Shapefile",overwrite_layer="TRUE")
+  
+  return(reg_outline_poly) #return spdf
+}
+
+#Function to aggregate from fine to coarse resolution, this will change accordingly once the input raster ref is given..
+#
+aggregate_raster <- function(agg_fact,r_in,reg_ref_rast=NULL,agg_fun="mean",out_suffix=NULL,file_format=".tif",out_dir=NULL){
+  #Aggregate raster from raster input and reference file
+  #INPUT arguments:
+  #agg_fact: factor to aggregate
+  #agg_fun: default is mean
+  #out_suffix: output suffix
+  #file_Format: raster format used e.g. .tif
+  #reg_ref_rast: reference raster to match in resolution, if NULL then send a message
+  #out_dir: output directory
+  #OUTPUT:
+  # raster_name: name of the file containing the aggregated raster
+  #
+  # Authors: Benoit Parmentier
+  # Created: 10/15/2015
+  # Modified: 11/01/2015
+  # To Do: 
+  # - Add option to disaggregate
+  #
+  ################################
+  
+  if(is.null(agg_fact)){
+    res_ref <- res(reg_ref_rast)[1] #assumes square cells, and decimal degrees from WGS84 for now...
+    res_in <- res(r_in)[1] #input resolution, assumes decimal degrees
+    agg_fact <-round(res_ref/res_in) #find the factor needed..
+    #fix this to add other otpions e.g. aggregating down
+  }
+  
+  #Default values...
+  if(is.null(out_suffix)){
+    out_suffix <- ""
+  }
+  
+  if(is.null(out_dir)){
+    out_dir <- "."
+  }
+  
+  raster_name <- file.path(out_dir,paste("r_agg_",agg_fact,out_suffix,file_format,sep="")) #output name for aster file
+  r_agg <- aggregate(r_in, fact=agg_fact,FUN=agg_fun,filename=raster_name,overwrite=TRUE)
+  
+  return(raster_name)
+  
+}
+
+### This is a very general function to process raster to match a raster of reference,
+create__m_raster_region <-function(j,list_param){
+  #This processes a list of raster to match to a region of interest defined by a reference raster
+  #INPUT Arguments: raster name of the file,reference file with
+  # j: file to be processed with input parameters
+  # raster_name: list of raster to process i.e. match the region of interest
+  # reg_ref_rast: reference raster used to defined the region of interest and spatial parameters
+  # out_rast_name: output raster name, if NULL then use out_suffix to the input name to generate output name
+  # agg_param: aggregation parameters: this is a vector used in in the aggregate function. It has three items:
+  #                                     -TRUE/FALSE: if true then aggregate
+  #                                     -agg_fact: aggregation factor, if NULL compute on the fly
+  #                                     -agg_fun: aggregation function to use, the default is mean
+  # file_format: output format used in the raster e.g. .tif, .rst
+  # NA_flag_val: flag value used for no data
+  # input_proj_str: defined projection,default null in which case it is extract from the input raster
+  # out_suffix : output suffix added to output names if no output raster name is given
+  # out_dir:  <- list_param$out_dir
+  # Output: spatial grid data frame of the subset of tiles
+  #
+  # Authors: Benoit Parmentier
+  # Created: 10/01/2015
+  # Modified: 11/01/2015
+  #TODO:
+  # - Add option to disaggregate...
+  # - Modify agg param to be able to use different ones by file j for the mcapply function
+  #
+  ################################################
+  ## Parse input arguments
+  raster_name <- list_param$raster_name[[j]] #list of raster ot project and crop, this is a list!!
+  reg_ref_rast <- list_param$reg_ref_rast #This must have a coordinate system defined!!
+  out_rast_name <- list_param$out_rast_name[j] #if NULL then use out_suffix to add to output name
+  agg_param <- list_param$agg_param #TRUE,agg_fact,agg_fun
+  file_format <- list_param$file_format #.tif, .rst
+  NA_flag_val <- list_param$NA_flag_val #flag value used for no data
+  input_proj_str <- list_param$input_proj_str #default null?
+  out_suffix <- list_param$out_suffix
+  out_dir <- list_param$out_dir
+  
+  ## Start #
+  
+  ## Create raster object if not already present
+  if(class(raster_name)!="RasterLayer"){
+    layer_rast<-raster(raster_name)
+  }else{
+    layer_rast <- raster_name
+    raster_name <- filename(layer_rast)
+  }
+  
+  ## Create output raster name if out_rast_name is null
+  if(is.null(out_rast_name)){
+    extension_str <- extension(raster_name)
+    raster_name_tmp <- gsub(extension_str,"",basename(raster_name))
+    if(out_suffix!=""){
+      out_rast_name <- file.path(out_dir,paste(raster_name_tmp,"_crop_proj_reg_",out_suffix,file_format,sep="")) #for use in function later...
+    }else{
+      out_rast_name <- file.path(out_dir,paste(raster_name_tmp,"_crop_proj_reg",out_suffix,file_format,sep="")) #for use in function later...
+    }
+  }
+  
+  ## Get the input raster projection information if needed
+  if(is.null(input_proj_str)){
+    input_proj_str <-projection(layer_rast)   #Extract current coordinates reference system in PROJ4 format
+  }else{
+    projection(layer_rast) <- input_proj_str #assign projection info
+  }
+  region_temp_projected <- projectExtent(reg_ref_rast,CRS(input_proj_str))     #Project from ref to current region coord. system
+  
+  layer_crop_rast <- crop(layer_rast, region_temp_projected) #crop using the extent from the region tile
+  #layer_projected_rast<-projectRaster(from=layer_crop_rast,crs=proj4string(reg_outline),method="ngb")
+  if(agg_param[1]==TRUE){
+    agg_fact <- as.numeric(agg_param[2]) #in case we have a string/char type
+    agg_fun <- agg_param[3]
+    #debug(aggregate_raster)
+    r_agg_raster_name <- aggregate_raster(reg_ref_rast, #reference raster with the desired resolution
+                                          agg_fact=agg_fact, #given aggregation factor
+                                          r_in=layer_crop_rast, #raster to be aggregated
+                                          agg_fun="mean", #aggregation function
+                                          out_suffix=out_suffix,
+                                          file_format=".tif",
+                                          out_dir=out_dir)
+    layer_crop_rast <- raster(r_agg_raster_name)
+  }
+  #Should check if different projection!!!
+  layer_projected_rast <- projectRaster(from=layer_crop_rast,to=reg_ref_rast,method="ngb",
+                                        filename=out_rast_name,overwrite=TRUE)
+  
+  #Need cleanup of tmp files here!!! building up to 19gb!
+  removeTmpFiles(h=0)
+  
+  return(out_rast_name)
+}
+
 ## Add processing of downloaded files!!
 ## 1) crop and reproject if needed
 ## 2) creation of TimeRaster object with summaries?
@@ -134,17 +311,18 @@ downloading_prism_product <- function(start_date, end_date,var_name,num_cores,ou
 
 in_dir <- "/home/bparmentier/Google Drive/NEST/" #local bpy50 , param 1
 #in_dir <- "/home/parmentier/Data/rainfall/NEST" #NCEAS, param 
-out_dir <- "/home/bparmentier/Google Drive/NEST/prism_tmin"
+out_dir <- "/home/bparmentier/Google Drive/NEST/"
 start_date <- "2011-01-01" #PARAM 12
 end_date <- "2011-12-31" #PARAM 13
 
 var_name <- "ppt" #tmin,tmax
 num_cores <- 4
-file_format <- ".rst" #PARAM 4
+file_format <- ".tif" #PARAM 4
 NA_value <- -9999 #PARAM5
 NA_flag_val <- NA_value #PARAM6
-out_suffix <-"NEST_prism_01142016" #output suffix for the files and ouptu folder #PARAM 7
-create_out_dir_param=FALSE #PARAM8
+out_suffix <-"NEST_prism_01212016" #output suffix for the files and ouptu folder #PARAM 7
+create_out_dir_param=TRUE #PARAM8
+ref_rast_name <- "/home/bparmentier/Google Drive/NEST/prism_rain/prismrain2012/prismrain_20120101.tif"
 
 out_dir <- in_dir #output will be created in the input dir
 out_suffix_s <- out_suffix #can modify name of output suffix
@@ -155,16 +333,54 @@ if(create_out_dir_param==TRUE){
   setwd(out_dir) #use previoulsy defined directory
 }
 
+########## PART 1: Download and unzip ##############
 #debug(downloading_prism_product) 
+setwd(in_dir)
 test <- downloading_prism_product(start_date, end_date,var_name,num_cores,out_dir=NULL)
+setwd(out_dir)
   
-##### Now unzip function
-unzip_lf_fun <- function(lf, out_dir){
-  #unzip PRISM_ppt_stable_4kmD2_20110102_bil.zip
-  unzip(lf,exdir=out_dir)
-}
+##### Now unzip by year ...
 
-la
+lf_zip <- file.path(test$dir,test$file_zip)
+lf_r <- lapply(lf_zip, unzip,exdir=out_dir)
+lf_r <- list.files(pattern="*bil.bil$",path=out_dir,full.names = T)
+
+########## PART 2: Match to study area ##############
+
+## Match to the study area...
+## Now crop and reproject if necessary
+#Use function above
+#
+r_ref <- raster(ref_rast_name)
+plot(r_ref)
+agg_param <- c(FALSE,NULL,"mean") #False means there is no aggregation!!!
+#use r_ref as reference...
+out_rast_name <- NULL
+list_param_create_region <- list(as.list(lf_r),
+                                 r_ref, out_rast_name,agg_param,
+                                 file_format,NA_flag_val,
+                                 input_proj_str=NULL,out_suffix="",out_dir)
+names(list_param_create_region) <- c("raster_name",
+                                     "reg_ref_rast", "out_rast_name","agg_param",
+                                     "file_format","NA_flag_val",
+                                     "input_proj_str","out_suffix","out_dir")
+#debug(create__m_raster_region)
+#test <- create__m_raster_region(1,list_param=list_param_create_region)
+
+#test <- create__m_raster_region(1,list_param=list_param_create_region)
+lf_r_reg <- mclapply(1:length(lf_r),
+                          FUN=create__m_raster_region,
+                          list_param=list_param_create_region,
+                          mc.preschedule=FALSE,
+                          mc.cores = num_cores)
+#Currently we use only this:
+
+rainfall <- stack(unlist(lf_r_reg ))
+plot(rainfall)
+
+########## PART 3: Match to study area ##############
+
+
 
 #################### END OF SCRIPT #####################
 
