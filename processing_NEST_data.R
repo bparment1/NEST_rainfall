295dbc9677a55194950705ba36e24883a7c32c3d
Benoit Parmentier <benoit.parmentier@gmail.com>
2016-01-24 10:45:00 -0500

    moving download and extract function to script function

--- processing_NEST_data.R
+++ processing_NEST_data.R
@@ -12,7 +12,7 @@
 #COMMENTS: -   
 #          - 
 #TO DO: DSS
-# -make this callable from shell?
+# - make this callable from shell using optparse package with documentation with usage
 #
 #################################################################################################
 
@@ -78,7 +78,7 @@ load_obj <- function(f){
 ## 3)
 ### Other functions ####
 
-function_processing_NEST_data <- "processing_NEST_data_function_01232016b.R" #PARAM 1
+function_processing_NEST_data <- "processing_NEST_data_function_01232016c.R" #PARAM 1
 script_path <- "/home/bparmentier/Google Drive/NEST/R_NEST" #path to script #PARAM 
 #script_path <- "/home/parmentier/Data/rainfall/NEST"
 source(file.path(script_path,function_processing_NEST_data)) #source all functions used in this script 1.
@@ -90,8 +90,8 @@ in_dir <- "/home/bparmentier/Google Drive/NEST/" #local bpy50 , param 1
 #in_dir <- "/home/parmentier/Data/rainfall/NEST" #NCEAS, param 
 out_dir <- "/home/bparmentier/Google Drive/NEST/" #param 2
 
-start_date <- "2003-12-17" # param 3
-end_date <- "2004-01-05" # param 4
+start_date <- "2014-12-26" # param 3
+end_date <- "201-01-05" # param 4
 
 var_name <- "ppt" #tmin,tmax #param 5
 num_cores <- 4 #param 6
@@ -119,124 +119,15 @@ if(create_out_dir_param==TRUE){
 #turn this into a function and download and process by year
 # do 2003 to 2014
 
-debug(download_and_process_prism_data) 
-download_and_process_prism_data(in_dir,out_dir,start_date,end_date,var_name,ref_rast_name,
+#debug(download_and_process_prism_data) 
+lf_reg <- download_and_process_prism_data(in_dir,out_dir,start_date,end_date,var_name,ref_rast_name,
                       agg_param,num_cores=1,create_out_dir_param=FALSE,NA_value=-9999,out_suffix="",
                       file_format=".tif",download_file=T,unzip_files=T,match_file=T,lf_zip=NULL,lf_r=NULL)
 
   
-download_and_process_prism_data <- function(in_dir,out_dir,start_date,end_date,var_name,ref_rast_name,
-         agg_param,num_cores=1,create_out_dir_param=FALSE,NA_value=-9999,out_suffix="",
-         file_format=".tif", download_file=T,unzip_files=T,match_file=T,lf_zip=NULL,lf_r=NULL){
-  
-  #INPUTS
-  #1)in_dir <- "/home/bparmentier/Google Drive/NEST/" #local bpy50 , param 1
-  #2)out_dir <- "/home/bparmentier/Google Drive/NEST/" #param 2
-  #3)start_date <- "2003-12-17" # param 3
-  #4)end_date <- "2004-01-05" # param 4
-  #5)var_name <- "ppt" #tmin,tmax #param 5
-  #6)num_cores <- 4 #param 6
-  #7)file_format <- ".tif" #param 7
-  #8)NA_value <- -9999 # param 8
-  #9)out_suffix <-"NEST_prism_01212016" #output suffix for the files and ouptu folder #param 9
-  #10)create_out_dir_param=TRUE # param 10
-  #11)ref_rast_name <- "/home/bparmentier/Google Drive/NEST/prism_rain/prismrain2012/prismrain_20120101.tif" #param 10
-  #12)agg_param <- c(FALSE,NULL,"mean") #False means there is no aggregation!!! #param 11
-  
-  
-  NA_flag_val <- NA_value 
-  
-  ######### PART 1: downloading prsim product ######
-  if(download_file==T){
-    setwd(in_dir)
-    #debug(downloading_prism_product)
-    download_obj <- downloading_prism_product(start_date, end_date,var_name,num_cores,in_dir=in_dir,out_dir=NULL)
-    setwd(out_dir)
-  }else{
-    download_obj<-try(load_obj(file.path(in_dir,paste("downloaded_prism_data_",var_name,".RData",sep=""))))
-  }
-  
-  ####### PART 2: extracting files #######
-
-  ## run by year!!
-  ## loop through year...or make this a function?
-  if(unzip_files==T){
-    if(is.null(lf_zip)){
-      lf_zip <- download_obj$lf_zip
-    }
-    l_year <- length(lf_zip)
-    list_lf_r <- vector("list",length=l_year)
-    for(i in 1:length(l_year)){
-        out_dir_year <- unique(dirname(lf_zip[[i]]))
-        lf_r <- lapply(lf_zip[[i]], unzip,exdir= out_dir_year)
-        lf_r <- list.files(pattern="*bil.bil$",path=out_dir_year,full.names = T)
-        list_lf_r[[i]] <- lf_r
-    }
-  }
-
-  ########## PART 3: Match to study area ##############
-    
-  ## Match to the study area...
-  ## Now crop and reproject if necessary
-  #Use function above
-  r_ref <- raster(ref_rast_name)
-  list_lf_r_reg <- vector("list",length(list_lf_r))
-  for(i in 1:length(list_lf_r)){
-    lf_r <- list_lf_r[[i]]
-    out_dir_year <- unique(dirname(lf_r))
-    #plot(r_ref)
-    #agg_param <- c(FALSE,NULL,"mean") #False means there is no aggregation!!!
-    #use r_ref as reference...
-    out_rast_name <- NULL
-    list_param_create_region <- list(as.list(lf_r),
-                                     r_ref, out_rast_name,agg_param,
-                                     file_format,NA_flag_val,
-                                     input_proj_str=NULL,out_suffix="",out_dir_year)
-    names(list_param_create_region) <- c("raster_name",
-                                         "reg_ref_rast", "out_rast_name","agg_param",
-                                         "file_format","NA_flag_val",
-                                         "input_proj_str","out_suffix","out_dir")
-    #debug(create__m_raster_region)
-    #test <- create__m_raster_region(1,list_param=list_param_create_region)
-    lf_r_reg <- mclapply(1:length(lf_r),
-                         FUN=create__m_raster_region,
-                         list_param=list_param_create_region,
-                         mc.preschedule=FALSE,
-                         mc.cores = num_cores)
-    #Currently we use only this:
-    
-    #rainfall <- stack(unlist(lf_r_reg ))
-    #plot(rainfall)
-    list_lf_r_reg[[i]] <- lf_r_reg
-  }
-  
-  ##########
-  return(list_lf_reg)
-}
-  
-
-
 
 #################### END OF SCRIPT #####################
 
-# use strict;
-# use warnings;
-# use DateTime;
-# day <- 
-# my $clim_var = 'ppt';
-# my $base_url = 'http://services.nacse.org/prism/data/public/4km';
-# my $start = DateTime->new( day => 1, month => 10, year => 1999 );
-# my $stop = DateTime->new( day => 30, month => 9, year => 2000 );
-# while($start <= $stop) {
-#   $day = $start->strftime('%Y%m%d'); #place date in proper form
-#   system("wget --content-disposition $base_url/$clim_var/$day");
-#   sleep 2; #to be nice to our server
-#   $start->add(days => 1);
-# }
-# 
-# 
-# dates_queried <- format(ll,"%Y.%m.%d") #formatting queried dates
-# 
 
 
 
