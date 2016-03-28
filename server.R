41d78390984130cee3e9f9c375e6a149e2ab6cf1
Benoit Parmentier <benoit.parmentier@gmail.com>
2016-03-28 10:45:00 -0400

    modifying time series plot in server to changes date range

--- server.R
+++ server.R
@@ -4,7 +4,7 @@
 
 #AUTHORS: Benoit Parmentier                                             
 #DATE CREATED: 03/12/2016 
-#DATE MODIFIED: 03/25/2016
+#DATE MODIFIED: 03/28/2016
 #Version: 1
 #PROJECT: NEST beach closures            
 
@@ -38,24 +38,67 @@ shinyServer(function(input, output) {
     #cars2 <- cars + rnorm(nrow(cars))
     #plot(cars2)
     #plot_ts <- 
+    if(input$dataset=="MHB"){
+      data_df <- data_df_MHB
+      rm(data_df_MHB)
+      rm(data_df_DMR)
+    }
+    if(input$dataset=="DMR"){
+      data_df <- data_df_DMR
+      rm(data_df_MHB)
+      rm(data_df_DMR)
+    }
+    
     id_name<-input$station
     id_selected <- data_df[[var_ID]]==id_name
     
     ### Not get the data from the time series
     data_pixel <- data_df[id_selected,]
-    data_pixel <- as.data.frame(data_pixel)
-    r_ts_name <- names(r_rainfall)
+    data_pixel$rainfall <- as.numeric(data_pixel$rainfall)
+    d_z_tmp <-zoo(data_pixel$rainfall,as.Date(data_pixel$date))
+    #names(d_z_tmp)<- "rainfall"
+    #data_pixel <- as.data.frame(data_pixel)
+    d_z_tmp2 <- zoo(data_pixel[[var_name]],as.Date(data_pixel$date))
     
-    var_pix_ts <- t(as.data.frame(subset(data_pixel,select=var_name)))
+    d_z <- window(d_z_tmp,start=start_date,end=end_date)
+    d_z2 <- window(d_z_tmp2,start=start_date,end=end_date)
+    df2 <- as.data.frame(d_z2)
+    names(df2)<- var_name
+    #d_z <- subset(d_z,select=c("rainfall"))
+    #d_z$rainfall <- as.numeric(as.character(d_z$rainfall))
+    #r_ts_name <- names(r_rainfall)
+    
+    #var_pix_ts <- t(as.data.frame(subset(data_pixel,select=var_name)))
     #pix <- t(data_pixel[1,24:388])#can subset to range later
     
-    pix_ts <- t(as.data.frame(subset(data_pixel,select=r_ts_name))) #can subset to range later
+    #pix_ts <- t(as.data.frame(subset(data_pixel,select=r_ts_name))) #can subset to range later
     #pix_ts <- subset(as.data.frame(pix_ts),select=r_ts_name)
-    pix_ts <- (as.data.frame(pix_ts))
+    #pix_ts <- (as.data.frame(pix_ts))
     ## Process the coliform data
-    d_z <- zoo(pix_ts,idx) #make a time series ...
-    names(d_z)<- "rainfall"
-    plot(d_z,lty=2,ylab="rainfall",xlab="Time",main=paste0("Rainfall for station ",id_name))
+    #d_z <- zoo(pix_ts,idx) #make a time series ...
+    #names(d_z_tmp)<- "rainfall"
+    #test<-as.data.frame(d_z)
+    #plot(test$rainfall,lty=2,ylab="rainfall",xlab="Time",main=paste0("Rainfall for station ",id_name))
+    #(start_date)
+    plot(d_z,lty=2,ylab="rainfall",xlab="Time",main=paste0("Rainfall from ",start_date," to ", end_date,
+                                                    " for station ",id_name))
+    
+    #abline(h=threshold_val,col="green")
+    par(new=TRUE)              # key: ask for new plot without erasing old
+    #plot(x,y,type="l",col=t_col[k],xlab="",ylab="",lty="dotted",axes=F) #plotting fusion profile
+    #plot(log(df2$COL_SCORE),pch=10,cex=2.5,col="red", axes=F,ylab="",xlab="")
+    plot(log(df2[[var_name]]),pch=10,cex=2.5,col="red", axes=F,ylab="",xlab="")
+    
+    #points(d_z2$COL_SCORE,col="red",pch=10,cex=2)
+    legend("topleft",legend=c("stations"), 
+           cex=1.2,col="red",pch =10,bty="n")
+    
+    axis(4,cex=1.2)
+    mtext(4, text = "coliform scores", line = 3)
+    
+    #title(paste("Station time series",id_name,sep=" "))
+    
+    
   })
   
   output$raster_map <- renderPlot({
