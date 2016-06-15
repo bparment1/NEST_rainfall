b9c38b439f192ddcd6345e8c0d8629a6c851b8b5
Benoit Parmentier <benoit.parmentier@gmail.com>
2016-06-15 10:45:00 -0400

    adding functions to read data and run model with log option

--- analyses_NEST_functions.R
+++ analyses_NEST_functions.R
@@ -45,6 +45,63 @@ library(shiny)
 
 ###### Functions used in this script sourced from other files
 
+#function_rainfall_time_series_NEST_analyses <- "rainfall_time_series_NEST_function_03272016.R" #PARAM 1
+#script_path <- "." #path to script #PARAM 
 
+#script_path <- "/home/bparmentier/Google Drive/NEST/NEST_stations_s06/" #path to script #PARAM 
+#script_path <- "/home/benoit/data/NEST_stations_s06" #on SSI server
+#setwd(script_path)
+
+#script_path <- "/home/parmentier/Data/rainfall/NEST"
+#source(file.path(script_path,function_rainfall_time_series_NEST_analyses)) #source all functions used in this script 1.
+
+##### Functions used in this script 
+
+create_dir_fun <- function(outDir,out_suffix){
+  #if out_suffix is not null then append out_suffix string
+  if(!is.null(out_suffix)){
+    out_name <- paste("output_",out_suffix,sep="")
+    outDir <- file.path(outDir,out_name)
+  }
+  #create if does not exists
+  if(!file.exists(outDir)){
+    dir.create(outDir)
+  }
+  return(outDir)
+}
+
+#Used to load RData object saved within the functions produced.
+load_obj <- function(f){
+  env <- new.env()
+  nm <- load(f, env)[1]
+  env[[nm]]
+}
+
+read_select_station <- function(file_name,selected_val,selected_col){
+   tb <- read.table(file_name,sep=",")
+   df <- subset(tb,tb[[selected_col]]==selected_val)
+   #df <- subset(tb,tb$ID_stat==selected_ID)
+   return(df)
+}
+
+run_simple_lm <- function(df,y_var_name,x_var_name,log_val=T){
+  #
+  #
+  #test <- lm(log1p(df$COL_SCORE) ~ log1p(df$rainfall),df)
+  if(log_val==T){
+    mod <- lm(log1p(df[[y_var_name]]) ~ log1p(df[[x_var_name]]),df)
+  }
+  if(log_val!=T){
+    mod <- lm(df[[y_var_name]] ~ df[[x_var_name]],df)
+  }
+
+  #plot(log1p(df[[y_var_name]]) ~ log1p(df[[x_var_name]])) #only two data points for 2003!!!
+  summary_mod_tb <- (summary(mod))
+  tb_coefficients <- as.data.frame(summary_mod_tb$coefficients)
+  #plot(mod)
+  #change name of rows and add n columns for the number of inputs in the model!
+
+  return(tb_coefficients)
+}
 
 ########################### End of script #####################################
