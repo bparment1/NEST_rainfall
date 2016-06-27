b40d2d95358b439110248a350b702c1b1023ab1b
Benoit Parmentier <benoit.parmentier@gmail.com>
2016-06-27 10:45:00 -0400

    modification of processing script and application to DMR data

--- processing_NEST_data.R
+++ processing_NEST_data.R
@@ -1,10 +1,12 @@
 ##############################################  NEST Beach closure project  #######################################
 ###########################################  Data preparation and download #######################################
 #This script download data and processes rasters to fit the Maine study area.
+#This function downloads and match the files to the study area.
+#files are reprojected if needed.
 
 #AUTHORS: Benoit Parmentier                                             
 #DATE CREATED: 11/05/2015 
-#DATE MODIFIED: 03/10/2016
+#DATE MODIFIED: 06/27/2016
 #Version: 1
 #PROJECT: NEST beach closures            
 
@@ -78,7 +80,7 @@ load_obj <- function(f){
 ## 3)
 ### Other functions ####
 
-function_processing_NEST_data <- "processing_NEST_data_function_01232016c.R" #PARAM 1
+function_processing_NEST_data <- "processing_NEST_data_function_06272016.R" #PARAM 1
 script_path <- "/home/bparmentier/Google Drive/NEST/R_NEST" #path to script #PARAM 
 #script_path <- "/home/parmentier/Data/rainfall/NEST"
 source(file.path(script_path,function_processing_NEST_data)) #source all functions used in this script 1.
@@ -90,18 +92,26 @@ in_dir <- "/home/bparmentier/Google Drive/NEST/" #local bpy50 , param 1
 #in_dir <- "/home/parmentier/Data/rainfall/NEST" #NCEAS, param 
 out_dir <- "/home/bparmentier/Google Drive/NEST/" #param 2
 
-start_date <- "2016-01-01" # param 3
-end_date <- "2016-03-09" # param 4
+start_date <- "2012-01-01" # param 3
+end_date <- "2012-12-31" # param 4
 
 var_name <- "ppt" #tmin,tmax #param 5
-num_cores <- 4 #param 6
-file_format <- ".tif" #param 7
-NA_value <- -9999 # param 8
-NA_flag_val <- NA_value 
-out_suffix <-"NEST_prism_03102016" #output suffix for the files and ouptu folder #param 9
-create_out_dir_param=TRUE # param 10
-ref_rast_name <- "/home/bparmentier/Google Drive/NEST/prism_rain/prismrain2012/prismrain_20120101.tif" #param 10
-agg_param <- c(FALSE,NULL,"mean") #False means there is no aggregation!!! #param 11
+ref_rast_name <- "/home/bparmentier/Google Drive/NEST/prism_rain/prismrain2012/prismrain_20120101.tif" #param 6
+agg_param <- c(FALSE,NULL,"mean") #False means there is no aggregation!!! #param 7
+
+num_cores <- 4 #param 8
+create_out_dir_param=TRUE # param 9
+NA_value <- -9999 # param 10
+NA_flag_val <- NA_value #param 11
+
+out_suffix <-"NEST_prism_06272016" #output suffix for the files and ouptu folder #param 12
+
+file_format <- ".tif" #param 13
+download_file <- FALSE #param 14
+unzip_files <- F #param 15
+match_file <- T #param 16
+lf_zip <- NULL #param 17
+lf_r <- NULL #param 18
 
 ######### PART 0: Set up the output dir ################
 
@@ -120,11 +130,15 @@ if(create_out_dir_param==TRUE){
 # do 2003 to 2014
 
 #debug(download_and_process_prism_data) 
-lf_reg <- download_and_process_prism_data(in_dir,out_dir,start_date,end_date,var_name,ref_rast_name,
-                      agg_param,num_cores=1,create_out_dir_param=FALSE,NA_value=-9999,out_suffix="",
-                      file_format=".tif",download_file=T,unzip_files=T,match_file=T,lf_zip=NULL,lf_r=NULL)
+#lf_reg <- download_and_process_prism_data(in_dir,out_dir,start_date,end_date,var_name,ref_rast_name,
+#                      agg_param,num_cores=1,create_out_dir_param=FALSE,NA_value=-9999,out_suffix="",
+#                      file_format=".tif",download_file=T,unzip_files=T,match_file=T,lf_zip=NULL,lf_r=NULL)
 
-  
+
+lf_reg <- download_and_process_prism_data(in_dir,out_dir,start_date,end_date,var_name,ref_rast_name,
+                                          agg_param,num_cores=1,create_out_dir_param=FALSE,NA_value=-9999,out_suffix="",
+                                          file_format=".tif",download_file=download_file,unzip_files=unzip_files,
+                                          match_file=match_file,lf_zip=lf_zip,lf_r=lf_r)
 
 #################### END OF SCRIPT #####################
 
