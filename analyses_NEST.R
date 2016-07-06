a54ef64d021de68696e2639a53ffed3750e63792
Benoit Parmentier <benoit.parmentier@gmail.com>
2016-07-06 10:45:00 -0400

    C rerunning model function script wiht new data a

--- analyses_NEST.R
+++ analyses_NEST.R
@@ -5,7 +5,7 @@
 
 #AUTHORS: Benoit Parmentier                                             
 #DATE CREATED: 06/14/2016 
-#DATE MODIFIED: 06/23/2016
+#DATE MODIFIED: 07/06/2016
 #Version: 1
 #PROJECT: NEST beach closures            
 
@@ -47,11 +47,11 @@ library(plyr)                                # Various tools including rbind.fil
 ###### Functions used in this script sourced from other files
 
 #function_rainfall_time_series_NEST_analyses <- "rainfall_time_series_NEST_function_03272016.R" #PARAM 1
-function_analyses_NEST <- "analyses_NEST_functions_06232016b.R" #PARAM 1
+function_analyses_NEST <- "analyses_NEST_functions_07062016.R" #PARAM 1
 
 #script_path <- "." #path to script #PARAM 
-#script_path <- "/home/parmentier/Data/NEST/R_NEST"
-script_path <- "/home/bparmentier/Google Drive/NEST/R_NEST" #path to script #PARAM 
+script_path <- "/home/parmentier/Data/NEST/R_NEST"
+#script_path <- "/home/bparmentier/Google Drive/NEST/R_NEST" #path to script #PARAM 
 #script_path <- "/home/bparmentier/Google Drive/NEST/NEST_stations_s06/" #path to script #PARAM 
 #script_path <- "/home/benoit/data/NEST_stations_s06" #on SSI server
 #setwd(script_path)
@@ -70,7 +70,8 @@ source(file.path(script_path,function_analyses_NEST)) #source all functions used
 #in_dir <- "/home/benoit/data/NEST_stations_s06" #U. Maine SSI server, param 
 #in_dir <- "." #Use current directory, run locally
 #in_dir <- "/home/parmentier/Data/NEST/NEST_stations_s08"
-in_dir <- "/home/bparmentier/Google Drive/NEST/NEST_stations_s08"
+in_dir <- "/home/parmentier/Data/NEST/data_analyses_NEST"
+#in_dir <- "/home/bparmentier/Google Drive/NEST/NEST_stations_s08"
 
 CRS_WGS84 <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +towgs84=0,0,0" #Station coords WGS84 # CONST 2
 proj_str<- CRS_WGS84 #param 2
@@ -79,15 +80,17 @@ CRS_reg <- CRS_WGS84 # PARAM 3
 file_format <- ".rst" #PARAM 4
 NA_value <- -9999 #PARAM5
 NA_flag_val <- NA_value #PARAM6
-out_suffix <-"NEST_prism_06232016b" #output suffix for the files and ouptu folder #PARAM 7
-#out_suffix <- paste0("NEST_analyses_",format(Sys.Date(),"%Y%m%d"))
+#out_suffix <-"NEST_prism_03282016" #output suffix for the files and ouptu folder #PARAM 7
+#out_suffix <- paste0(as.Date(Sys.Date(),format="%Y%m%d"))
+out_suffix <- paste0("NEST_analyses_",format(Sys.Date(),"%Y%m%d"))
 
 create_out_dir_param=T #PARAM8
 num_cores <- 11 #PARAM 9
 
 #rainfall_dir <- "/home/bparmentier/Google Drive/NEST_Data" #PARAM 10
 rainfall_dir <- "./data" #PARAM 10
-reg_ref_rast_name <- "./data/PRISM_ppt_stable_4kmD2_20111222_crop_proj_reg.tif"
+#reg_ref_rast_name <- "./data/PRISM_ppt_stable_4kmD2_20111222_crop_proj_reg.tif"
+reg_ref_rast_name <- "PRISM_ppt_stable_4kmD2_20111222_crop_proj_reg.tif"
 #station_data_fname <- file.path("/home/bparmentier/Google Drive/NEST_Data/", "WQ_TECS_Q.txt") #PARAM 11
 #station_data_fname <- file.path("data", "MHB_data_2006-2015.csv") #PARAM 11
 
@@ -95,19 +98,26 @@ reg_ref_rast_name <- "./data/PRISM_ppt_stable_4kmD2_20111222_crop_proj_reg.tif"
 #station_measurements_MHB_data_fname <- file.path("data", "data_df_rainfall_and_measurements_MHB.txt") #PARAM 11 
 #This will change
 #station_measurements_DMR_data_fname <- file.path("data", "data_df_rainfall_and_measurements_DMR.txt") #PARAM 11 
-station_measurements_DMR_data_fname <- list.files(path=file.path(in_dir,"data"),
+#station_measurements_DMR_data_fname <- list.files(path=file.path(in_dir,"data"),
+#                                                  pattern="data_df_combined_.*._DMR.txt",
+#                                                  full.names=T) #PARAM 11 
+#station_measurements_MHB_data_fname <- list.files(path=file.path(in_dir,"data"),
+#                                                  pattern="data_df_.*._NEST_prism_MHB.txt",
+#                                                  full.names=T) #PARAM 11 
+
+station_measurements_DMR_data_fname <- list.files(path=in_dir,
                                                   pattern="data_df_combined_.*._DMR.txt",
                                                   full.names=T) #PARAM 11 
-station_measurements_MHB_data_fname <- list.files(path=file.path(in_dir,"data"),
+station_measurements_MHB_data_fname <- list.files(in_dir,
                                                   pattern="data_df_.*._NEST_prism_MHB.txt",
                                                   full.names=T) #PARAM 11 
-
 #data_df_2015_NEST_prism_MHB.txt
 start_date <- "2012-01-01" #PARAM 12,  this is the default value, use user define otherwise
 end_date <- "2012-12-31" #PARAM 13,  this is the default rer define otherwise
 var_name_DMR <- "COL_SCORE" #PARAM 14
 var_name_MHB <- "CONCENTRATION" #PARAM 14, MHB data, need to add DMR
-var_name <- var_name_MHB
+#var_name <- var_name_MHB #default for shiny app
+var_name <- var_name_DMR #default for anaylses
 
 ##Raster start date
 start_date_r <- start_date #set the same start date for now
@@ -118,7 +128,8 @@ year_processed <- "2012" #PARAM 16
 threshold_val <- 2*25.4 #PARAM 17, in inches or mm
 convert_to_inches <- FALSE #PARAM 18
 units_val <- "mm"
-data_type <- "MHB" #for Maine beach health used as default, it is user defined otherwise
+#data_type <- "MHB" #for Maine beach health used as default, it is user defined otherwise
+data_type <- "DMR" #for Maine beach health used as default, it is user defined otherwise
 
 ## Change coordinates to x and y and lat long!!!
 coord_names_MHB <- c("SITE.LONGITUDE..UTM.","SITE.LATITUDE..UTM.") #MH beach bacteria dataset
@@ -129,6 +140,7 @@ coord_names_DMR <- c("LONGITUDE_DECIMAL","LATITUDE_DECIMAL") #cloroforms beach b
 SMAZones_fname <- "SMAZoneDissolve.shp"
 
 dat_stat_location_DMR_fname <- "dat_stat_location_DMR.shp" #Use this for now
+#dat_stat_location_DMR.shp
 dat_stat_location_MHB_fname <- "dat_stat_location_MHB.shp" #Use this for now
 
 transform_fun <- "log1p"
@@ -153,15 +165,17 @@ if(create_out_dir_param==TRUE){
 
 #data_df <- data_df_MHB #default dataset!!
 #data_df_DMR
-dat_stat_location_MHB <- readOGR(file.path(in_dir,"/data"),sub(".shp","",dat_stat_location_MHB_fname))
-dat_stat_location_DMR <- readOGR(file.path(in_dir,"/data"),sub(".shp","",dat_stat_location_DMR_fname))
+#dat_stat_location_MHB <- readOGR(file.path(in_dir,"/data"),sub(".shp","",dat_stat_location_MHB_fname))
+#dat_stat_location_DMR <- readOGR(file.path(in_dir,"/data"),sub(".shp","",dat_stat_location_DMR_fname))
+dat_stat_location_MHB <- readOGR(in_dir,sub(".shp","",dat_stat_location_MHB_fname))
+dat_stat_location_DMR <- readOGR(in_dir,sub(".shp","",dat_stat_location_DMR_fname))
 
-#data_type <- "DMR"
-data_type <- "MHB"
-list_ID <- unique(dat_stat_location_MHB$ID_stat)
+data_type <- "DMR"
+list_ID <- unique(dat_stat_location_DMR$ID_stat)
 
-tb <- read.table(station_measurements_MHB_data_fname[10],sep=",")
-list_ID <- unique(tb$ID_stat)
+tb <- read.table(station_measurements_DMR_data_fname[10],sep=",")
+#list_ID <- unique(tb$ID_stat)
+list_ID <- as.character(unique(tb[[var_ID]]))
 
 selected_ID <- list_ID[1]
 #selected_ID <- list_ID[5]  
@@ -169,7 +183,9 @@ selected_ID <- list_ID[1]
 #tb$ID_stat
 
 selected_val <- selected_ID
-selected_col <- "ID_stat"
+#selected_col <- "ID_stat"
+selected_col <- var_ID #ID_stat or LOCATION_ID
+
 #undebug(read_select_station)
 #df <- read_select_station(station_measurements_DMR_data_fname[10],selected_val,selected_col)
 
@@ -222,6 +238,28 @@ out_suffix_str <- paste0(data_type,"_",out_suffix)
 #                      mc.preschedule=FALSE,
 #                      mc.cores = num_cores)
 #19h10
+#debug(run_lm_by_station)
+#test <- run_lm_by_station(list_ID[1],                     
+#                     selected_col=selected_col,
+#                     x_var_name=x_var_name,
+#                     y_var_name=y_var_name,
+#                     lf=lf,
+#                     log_val=T,
+#                     out_dir=out_dir,
+#                     out_suffix=out_suffix,
+#                     num_cores=1)
+run_lm_obj <- mclapply(list_ID[1:11],
+                     FUN=run_lm_by_station,
+                     selected_col=selected_col,
+                     x_var_name=x_var_name,
+                     y_var_name=y_var_name,
+                     lf=lf,
+                     log_val=T,
+                     out_dir=out_dir,
+                     out_suffix=out_suffix,
+                     num_cores=1,
+                     mc.preschedule=FALSE,
+                     mc.cores = num_cores)
 run_lm_obj <- mclapply(list_ID,
                      FUN=run_lm_by_station,
                      selected_col=selected_col,
@@ -234,16 +272,27 @@ run_lm_obj <- mclapply(list_ID,
                      num_cores=1,
                      mc.preschedule=FALSE,
                      mc.cores = num_cores)
-
 save(run_lm_obj,
      file=file.path(out_dir,paste0("run_lm_obj_",data_type,"_",out_suffix,".RData")))
+save(run_lm_obj,
+     file=file.path(out_dir,paste0("test_",data_type,"_",out_suffix,".RData")))
 
 ### TO DO CHANGE THE TRANSFORMATION TO LOG10 OR SOME OTHER!!!
 
-run_lm_obj[[1]]$plot
-run_lm_obj[[1]]$tb_coefficients
+run_lm_obj[[10]]$plot
+run_lm_obj[[7]]$tb_coefficients
+run_lm_obj[[7]]
 
-l_tb_coef <- lapply(run_lm_obj,FUN=function(x){x[["tb_coefficients"]]})
+#l_tb_coef <- try(lapply(run_lm_obj,FUN=function(x){x[["tb_coefficients"]]}))
+#test <- mclapply(run_lm_obj[1701:1711],
+#         FUN=function(x){x[["tb_coefficients"]]},
+#                            mc.preschedule=FALSE,
+#                   mc.cores = 11)
+test <- mclapply(run_lm_obj,
+                 FUN=function(x){x[["tb_coefficients"]]},
+                 mc.preschedule=FALSE,
+                 mc.cores = 11)
+l_tb_coef <- test
 names(l_tb_coef) <- list_ID
 l_tb_coef_NA <- remove_from_list_fun(l_tb_coef)$list
 tb_coef_combined <- do.call(rbind.fill,l_tb_coef_NA) #create a df for NA tiles with all accuracy metrics
@@ -256,6 +305,17 @@ list_ID_col_tmp <- unlist(lapply(1:length(l_tb_coef_NA),
 tb_coef_combined$ID_stat <- list_ID_col_tmp
 tb_coef_combined$data_type <- data_type
 tb_coef_combined$transform_fun <- transform_fun
+
+#df_combined <- do.call(rbind,l_df) 
+write.table(tb_coef_combined,paste("tb_coef_combined_station_",out_suffix,".txt",sep=""),sep=",")
+#out_suffix_str <- paste(selected_ID,"_",out_suffix,sep="")
+
+##
+tb_slope <- subset(tb_coef_combined,coef_type="slope",select())
+tb_slope <- tb_coef_combined[tb_coef_combined$coef_type=="slope",]
+
+histogram(tb_slope$p)
+histogram(tb_slope$n)
 
 ########################### End of script #####################################
 
